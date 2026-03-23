ec63bc197d78269c971bf29bbd51973ae32bb4b4
jjarndt <jjarndt@users.noreply.github.com>
2026-03-23 11:23:22 +0100

    fix: restore broken SVG path in context pad icon
    The arc command lost a parameter when the HTML string was split into
    an array across lines: `a2 2 0 0 0 2-2` became `a2 2 0 0 2-2`.
    Restored the original single-line HTML string.

--- client/bpmn-extension/CallActivityContextPadProvider.js
+++ client/bpmn-extension/CallActivityContextPadProvider.js
@@ -2,13 +2,7 @@ import { getCalledProcessId, isCallActivity } from './util.mjs';
 
 const PROVIDER_PRIORITY = 900;
 
-const OPEN_EXTERNAL_ICON = [
-  '<div class="entry" style="display:flex;align-items:center;justify-content:center;">',
-  '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">',
-  '<path fill="#000" d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14',
-  'a2 2 0 0 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>',
-  '</svg></div>'
-].join('');
+const OPEN_EXTERNAL_ICON = '<div class="entry" style="display:flex;align-items:center;justify-content:center;"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#000" d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg></div>';
 
 export default class CallActivityContextPadProvider {
   constructor(contextPad, eventBus) {
